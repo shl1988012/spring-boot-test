5c996e97f9a5264cbbcdf9598fdf144b6774e3ca
hengshi <hengshi@cisco.com>
2019-08-09 09:07:38 +0800

    spring feature

--- src/main/java/com/spring/test/datasource/ReplaceSource.java
+++ src/main/java/com/spring/test/datasource/ReplaceSource.java
@@ -1,6 +1,9 @@
 package com.spring.test.datasource;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.AbstractResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,4 +20,16 @@ public class ReplaceSource extends AbstractResource {
     public InputStream getInputStream() throws IOException {
         return null;
     }
+
+
+    public void test(){
+        ClassPathResource resource = new ClassPathResource("bean.xml");
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(resource);
+    }
+
+
+
+
 }
